0bdf89a1c627cfa174961e094b42a52aaa1b35f2
Reginaldo <reginaldo007oliveira@gmail.com>
2021-10-20 22:44:49 -0300

    Redirect Ok

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
+import "dotenv/config"
 import  Express  from "express";
 const app = Express();
+
+app.get("/github", (request, response) => {
+    response.redirect(`http://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+
+})
 
 app.listen(4000, () => console.log(`   Server is running on PORT 4000`));
