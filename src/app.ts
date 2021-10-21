d416ae2b439f35d49f33ea9826adbeb0362f8de9
Reginaldo <reginaldo007oliveira@gmail.com>
2021-10-21 13:49:52 -0300

    Dependences Controllers Server Ok

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import "dotenv/config"
-import  Express  from "express";
-import { request } from "http";
-const app = Express();
+import  express  from "express";
+import { router} from "./routes";
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.use(router);
 
 app.get("/github", (request, response) => {
     response.redirect(`http://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`);
